55fcdbf57d2b6b73e4edb1835f7449f9bab3ad03
EmmonsCurse <1577972691@qq.com>
2024-01-30 15:15:28 +0800

    Add tile_op into op_teller's int8 list

--- paddle/fluid/inference/tensorrt/op_teller.cc
+++ paddle/fluid/inference/tensorrt/op_teller.cc
@@ -46,6 +46,7 @@ struct SimpleOpTypeSetTeller : public Teller {
 #endif
 #if IS_TRT_VERSION_GE(7000)
     teller_set.insert("tile");
+    int8_teller_set.insert("tile");
     teller_set.insert("flatten_contiguous_range");
     int8_teller_set.insert("flatten_contiguous_range");
     teller_set.insert("rnn");
@@ -2223,15 +2224,20 @@ struct SimpleOpTypeSetTeller : public Teller {
       if (!with_dynamic_shape) {
         if (tile_inputs.find("repeat_times_tensor") != tile_inputs.end()) {
           if (desc.Input("repeat_times_tensor").size() >= 1) {
+            VLOG(3) << "Tile op: repeat_times_tensor is not empty.";
             return false;
           }
         }
         if (tile_inputs.find("RepeatTimes") != tile_inputs.end()) {
           if (desc.Input("RepeatTimes").size() >= 1) {
+            VLOG(3) << "Tile op: RepeatTimes is not empty.";
             return false;
           }
         }
-        if (!desc.HasAttr("repeat_times")) return false;
+        if (!desc.HasAttr("repeat_times")) {
+          VLOG(3) << "Tile op:`repeat_times` is not set.";
+          return false;
+        }
       }
     }
 #endif
